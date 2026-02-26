17a67593af3b31c3826e94e4e88897e746ec0928
Thea Dubois <duboismariethea@gmail.com>
2026-02-26 00:33:00 +0000

    support multi frog

--- js/rules.js
+++ js/rules.js
@@ -11,9 +11,33 @@ function parseTextToRules(text) {
 
   const frogKeywords = ["frog", "toad", "animal", "creature", "froggy"];
   const rippleKeywords = ["ripple", "disturb", "break", "move", "movement", "waves", "rings", "vibrate", "vibration", "circle"];
+  const frogMentions = words.filter((w) => frogKeywords.some((k) => w.includes(k))).length;
+  const explicitFrogCount = lower.match(/\b(\d+)\s+(?:frog|frogs|toad|toads)\b/);
+  const hasPluralFrogWord = words.includes("frogs") || words.includes("toads");
+  const hasMultipleFrogPhrase = hasAnyPhrase([
+    "multiple frogs",
+    "many frogs",
+    "several frogs",
+    "a few frogs",
+    "couple of frogs",
+    "group of frogs",
+    "lots of frogs",
+    "multiple toads",
+    "many toads",
+    "several toads",
+  ]);
+
+  let frogs = frogMentions;
+  if (explicitFrogCount) {
+    frogs = Number(explicitFrogCount[1]);
+  } else if (frogMentions > 0 && hasMultipleFrogPhrase) {
+    frogs = Math.max(frogMentions, 3);
+  } else if (frogMentions > 0 && hasPluralFrogWord) {
+    frogs = Math.max(frogMentions, 2);
+  }
 
   return {
-    frogs: words.filter((w) => frogKeywords.some((k) => w.includes(k))).length,
+    frogs,
     frogSize: hasAnyPhrase(["tiny", "mini", "baby frog", "small"]) ? "small"
       : hasAnyPhrase(["big", "large", "huge", "ginormous", "daddy frog", "mommy frog"]) ? "large"
       : "normal",
